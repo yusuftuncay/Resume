e8339238937843c51674eeccf976e549d5c5a22c
yusuftuncay <yusufgenk@yahoo.com>
2023-05-11 02:10:52 +0200

    Added Interests Card and Many More

--- index.js
+++ index.js
@@ -43,9 +43,51 @@ function DarkMode() {
 
     /* Footer Icon */
     if (shadows[0].classList.contains("box-shadow")) {
-        document.getElementById("darkModeIcon").src = "img/light.png";
+        document.getElementById("darkModeIcon").src = "img/light-mode-sun.png";
+        document.getElementById("arrowIcon").src = "img/light-mode-arrow.png";
     }
     else if (shadows[0].classList.contains("box-shadow-dark")) {
-        document.getElementById("darkModeIcon").src = "img/dark.png";
+        document.getElementById("darkModeIcon").src = "img/dark-mode-sun.png";
+        document.getElementById("arrowIcon").src = "img/dark-mode-arrow.png";
     }
+}
+
+/* Define a flag variable to keep track of the current state of the image */
+var isFlipped = false;
+
+/* Define a function called flipImage() */
+function FlipImage() {
+    /* Retrieve element with the ID */
+    var image = document.getElementById("arrowIcon");
+    var interests = document.getElementById("interests");
+
+    /* Check if the image is flipped */
+    if (isFlipped) {
+        /* If the image is flipped, set the CSS transform property to rotate 0 degrees */
+        image.style.transform = "rotate(0deg)";
+
+        /* Hide the interests element */
+        interests.style.display = "none";
+        interests.style.opacity = 0;
+        interests.style.height = 0;
+        interests.style.margin = "8px 0px";
+
+        /* Update the flag variable to indicate that the image is no longer flipped */
+        isFlipped = false;
+    } else {
+        /* If the image is not flipped, set the CSS transform property to rotate 180 degrees */
+        image.style.transform = "rotate(180deg)";
+
+        /* Show the interests element */
+        interests.style.display = "block";
+        interests.style.opacity = 1;
+        interests.style.height = "auto";
+        interests.style.margin = "0px !important";
+
+        /* Update the flag variable to indicate that the image is flipped */
+        isFlipped = true;
+    }
+
+    /* Apply a transition to the transform property to smoothly animate the rotation */
+    image.style.transition = "transform 0.3s ease";
 }
